216477de3ee257ee4b4faedbe221b1d5f31cb279
huashenliang <JohnnyLiang77@gmail.com>
2020-05-15 17:33:28 -0500

    testing 5

--- index.js
+++ index.js
@@ -63,6 +63,6 @@ app.get('*', (req, res) => {
 });
 
 const port = process.env.PORT || 5000;
-app.listen(3000);
+// app.listen(port);
 
-server.listen(PORT, () => console.log(`Server has started at ${PORT}.`));
+server.listen(port);
